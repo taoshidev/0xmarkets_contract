863627e080758834e547472d8ffe225412ba3c6b
Rene <135692166+renesweet24@users.noreply.github.com>
2025-10-02 18:31:05 +0000

    chore: remove uncessary files

--- deploy/deployGlvStoreUtils.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
-
-// Disabled: GLV Store utils not present in this repo snapshot
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  void hre;
-};
-func.skip = async () => true;
-export default func;
